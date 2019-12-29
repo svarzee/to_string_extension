432ba82ffc18783aa88268772e0dd36dd2dfc54f
svarzee <svarzee@gmail.com>
2019-12-30 00:07:58 +0100

    Fix generator order

--- to_string_extension/lib/to_string_extension.dart
+++ to_string_extension/lib/to_string_extension.dart
@@ -4,4 +4,4 @@ import 'package:class_extensions/class_extensions.dart';
 import 'src/to_string_generator.dart';
 
 Builder toStringDummyBuilder(BuilderOptions options) =>
-    registerClassExtensionGenerator(1, ToStringGenerator());
+    registerClassExtensionGenerator(2, ToStringGenerator());
